ae56922a9ed5e8b474ca385f8674aed55f1b46bd
pridhi20 <115876281+pridhi20@users.noreply.github.com>
2025-07-25 15:28:36 +0530

    Add files via upload

---
+++ FoodDisplay.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import'./FoodDisplay.css';
+import { StoreContext } from '../../Context/StoreContext';
+import { useContext } from 'react';
+
+import FoodItem from '../FoodItem/FoodItem';
+
+const FoodDisplay = ({category}) => {
+  
+  const {food_list}=useContext(StoreContext)
+  
+    return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item,index)=>{
+              if(category=== "All"||category===item.category){
+                return <FoodItem
+  key={index}
+  id={item._id}
+  name={item.name}
+  discription={item.discription}
+  image={item.image}
+/>
+              }
+                
+            })}
+        </div>
+      
+    </div>
+  )
+}
+
+export default FoodDisplay
